1057b0389dacb7b41c57e660199da43ae57183a7
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2025-07-02 20:36:42 +0000

    wip: Maybe like this?

--- app/src/main/java/com/example/project_shelf/framework/ui/components/SearchTopBar.kt
+++ app/src/main/java/com/example/project_shelf/framework/ui/components/SearchTopBar.kt
@@ -3,17 +3,25 @@ package com.example.project_shelf.framework.ui.components
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.example.project_shelf.R
 
@@ -25,20 +33,31 @@ fun SearchTopBar(
     sharedTransitionScope: SharedTransitionScope,
 ) {
     CenterAlignedTopAppBar(
+        expandedHeight = 68.dp,
         title = {
-            // https://m3.material.io/components/search/specs
-            Surface(
-                shape = MaterialTheme.shapes.extraLarge,
-                onClick = onClick,
-                color = MaterialTheme.colorScheme.surfaceContainerHigh,
+            SearchBar(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-            ) {
-                with(sharedTransitionScope) {
-                    Text(stringResource(R.string.search))
-                }
-            }
+                    .height(60.dp)
+                    .padding(bottom = 8.dp),
+                inputField = {
+                    SearchBarDefaults.InputField(
+                        query = "",
+                        onQueryChange = {},
+                        onSearch = {},
+                        expanded = false,
+                        onExpandedChange = {},
+                        placeholder = { Text(stringResource(R.string.search)) },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.magnifying_glass_solid),
+                                contentDescription = null
+                            )
+                        },
+                    )
+                },
+                expanded = false,
+                onExpandedChange = {},
+            ) {}
         },
         scrollBehavior = scrollBehavior,
     )
